a78fabe3039d88bbebd864d59bead74e49543bd5
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-03-15 18:12:56 +0100

    add project as a the solution to U3_MVCFourierRechner_Muster

---
+++ U3_MVCFourierRechner_Muster/src/PropertyPanel.java
@@ -0,0 +1,75 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import util.MyBorderFactory;
+import util.Observable;
+import util.TraceV4;
+
+public class PropertyPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private TraceV4 trace = new TraceV4(this);
+
+	public JTextField tfEff = new JTextField(10);
+	public JTextField tfPeak = new JTextField(10);
+	public JTextField tfCrest = new JTextField(10);
+
+	/**
+	 * <pre>
+	 *  
+	 * - platiert die Elemente im GridbagLayout
+	 * - Textfelder sollen horizontal wachsen
+	 * - Labels sollen unverändert bleiben
+	 * - Als letzte Zeile eine in beide Richtung wachsende Leerzeile ergaenzen
+	 * </pre>
+	 */
+	public PropertyPanel() {
+		trace.constructorCall();
+		setLayout(new GridBagLayout());
+		setBorder(MyBorderFactory.createMyBorder(" Signaleigenschaften "));
+
+		add(new JLabel("Effektiwert"), new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(10, 10, 10, 10), 0, 0));
+		add(tfEff, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+				new Insets(10, 10, 10, 10), 0, 0));
+		tfEff.setEditable(false);
+
+		add(new JLabel("Spitzenwert"), new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(10, 10, 10, 10), 0, 0));
+		add(tfPeak, new GridBagConstraints(1, 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+				new Insets(10, 10, 10, 10), 0, 0));
+		tfPeak.setEditable(false);
+
+		add(new JLabel("Crest-Faktor"), new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.NONE, new Insets(10, 10, 10, 10), 0, 0));
+		add(tfCrest, new GridBagConstraints(1, 2, 1, 1, 1.0, 0.0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(10, 10, 10, 10), 0, 0));
+		tfCrest.setEditable(false);
+
+		add(new JLabel(), new GridBagConstraints(0, 4, 2, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
+				new Insets(10, 10, 10, 10), 0, 0)); // leerzeile
+
+	}
+
+	/**
+	 * - holt sich die aktuellen Daten aus Modell zum update der Elemente
+	 * 
+	 * @param obs
+	 * @param obj
+	 */
+	public void update(Observable obs, Object obj) {
+		trace.methodeCall();
+		if (obs instanceof Model) {
+			Model model = (Model) obs;
+			double rms = model.getRms();
+			double peak = model.getPeak();
+			tfPeak.setText(Double.toString(peak));
+			tfEff.setText("" + rms);// so gehts auch!
+			tfCrest.setText("" + (peak / rms));
+		}
+	}
+}
---
+++ U3_MVCFourierRechner_Muster/src/View.java
@@ -0,0 +1,65 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+
+import util.Observable;
+import util.Observer;
+import util.TraceV4;
+
+public class View extends JPanel implements Observer {
+	private static final long serialVersionUID = 1L;
+	private TraceV4 trace = new TraceV4(this);
+
+	
+	public ParameterPanel parameterPanel;
+	private PropertyPanel propertyPanel;
+	private PlotPanel plotPanel;
+	public ButtonPanel buttonPanel;
+
+	/** 
+	 * <pre>
+	 * - erzeugt die Panels und platziert im GridbagLayout
+	 * - Panels sollen bei Vergroesserung mitwachsen
+	 * </pre>
+	 */
+	public View(Controller controller) {
+		trace.constructorCall();
+		setLayout(new GridBagLayout());
+
+		
+		parameterPanel = new ParameterPanel(controller);
+		propertyPanel = new PropertyPanel();
+		plotPanel = new PlotPanel();
+		buttonPanel = new ButtonPanel(controller);
+
+		add(parameterPanel, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.BOTH, new Insets(10, 10, 10, 10), 0, 0));
+
+		add(propertyPanel, new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.BOTH, new Insets(10, 10, 10, 10), 0, 0));
+
+		add(plotPanel, new GridBagConstraints(0, 1, 2, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
+				new Insets(10, 10, 10, 10), 0, 0));
+
+		add(buttonPanel, new GridBagConstraints(0, 2, 2, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH,
+				new Insets(10, 10, 10, 10), 0, 0));
+
+	}
+
+	/**
+	 * <pre>
+	 * - ruft die update Methode der entsprechenden Panels auf
+	 * 
+	 * </pre>
+	 */
+	@Override
+	public void update(Observable obs, Object obj) {
+		trace.methodeCall();
+		
+		propertyPanel.update(obs, obj);
+		plotPanel.update(obs, obj);
+
+	}
+}

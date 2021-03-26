c0b5f9e4808fad5e4fccc3e78791789131378976
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-03-26 13:13:16 +0100

    update U5

---
+++ Demo_GUI/src/JUnit_Test_Gui.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class JUnit_Test_Gui {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

1f36164c9b24fb52aa31620b43600b1be6beeacd
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-03-27 11:27:23 +0100

    edit dice to have better aspect

--- roll-the-dice/src/View.java
+++ roll-the-dice/src/View.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -7,6 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.plaf.FontUIResource;
 
 public class View extends JPanel {
 
@@ -17,7 +20,7 @@ public class View extends JPanel {
 	public void setController(Controller controller) {
 		this.controller = controller;
 	}
-
+	
 	
 
 	private JLabel lbDice = new JLabel("-");
@@ -26,6 +29,11 @@ public class View extends JPanel {
 
 	public void init() {
 		setLayout(new GridBagLayout());
+		
+		lbDice.setFont(new Font("Monospaced",Font.PLAIN, 40));
+		lbDice.setOpaque(true);
+		lbDice.setBackground(Color.DARK_GRAY);
+		lbDice.setForeground(Color.WHITE);
 
 		add(lbDice, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.NONE,
 				insets, 0, 0));
@@ -45,6 +53,6 @@ public class View extends JPanel {
 	}
 
 	public void update(Model model) {
-		lbDice.setText(model.dice+ "");			
+		lbDice.setText(" "+model.dice+ " ");			
 	}
 }

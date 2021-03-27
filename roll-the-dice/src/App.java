3b25519ece38a0493f4f46ff68678ea077d5c785
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-03-27 10:22:16 +0100

    app class complete

--- roll-the-dice/src/App.java
+++ roll-the-dice/src/App.java
@@ -1,8 +1,36 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		 
+		JFrame frame= new JFrame("Roll the Dice");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		final View panel= new View();
+		panel.setPreferredSize(new Dimension(300, 200));
+		panel.setDoubleBuffered(true);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(true);
+		frame.setLocationRelativeTo(null);
+		
+		
+		panel.init();
+		panel.setFocusable(true);
+		panel.requestFocus();
+		panel.invalidate();
+		 
 
 	}
 
--- roll-the-dice/src/View.java
+++ roll-the-dice/src/View.java
@@ -1,8 +1,27 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class View extends JPanel{
 	
+	private JLabel lbDice= new JLabel("-");
+	private JButton btRollButton= new JButton("Roll");
+	private Insets insets= new Insets(5, 5, 5, 5);
+	
 	public void init() {
+		setLayout(new GridBagLayout());
+		
+		add(lbDice, new GridBagConstraints(0 ,0 , 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, 
+				insets, 0, 0));
+		
+		add(btRollButton, new GridBagConstraints(0 ,1 , 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, 
+				insets, 0, 0));
+		
+		
 		
 	}
 	

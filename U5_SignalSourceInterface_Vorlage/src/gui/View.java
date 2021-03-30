9c704861b3a14e3cb39e615d143f216ed84230c3
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-03-30 05:36:04 +0200

    edit class view: creats panels, update panels

--- U5_SignalSourceInterface_Vorlage/src/gui/View.java
+++ U5_SignalSourceInterface_Vorlage/src/gui/View.java
@@ -11,6 +11,8 @@ import util.Observer;
 public class View extends JPanel implements Observer {
 	private static final long serialVersionUID = 1L;
 
+	private static final Insets insets = new Insets(10, 10, 10, 10);
+
 	public ParameterPanel parameterPanel;
 	public PropertyPanel propertyPanel = new PropertyPanel();
 	public PlotPanel plotPanel = new PlotPanel();
@@ -18,18 +20,22 @@ public class View extends JPanel implements Observer {
 	public View(Controller controller) {
 		super(new GridBagLayout());
 		setBorder(MyBorderFactory.createMyBorder(" View Panel "));
+		
+		parameterPanel = new ParameterPanel(controller);
+		
+		add(plotPanel, new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				insets, 0, 0));
 
-		
-		
-		
+		add(parameterPanel, new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, insets, 0, 0));
 
-		
-
+		add(propertyPanel, new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, insets, 0, 0));
 	}
 
 	public void update(Observable obs, Object obj) {
-		
-
+		plotPanel.update(obs, obj);
+		propertyPanel.update(obs, obj);
 	}
 
 }

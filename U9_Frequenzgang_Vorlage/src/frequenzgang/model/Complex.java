63e19828e6380fdfa11278476e44a8fd05beda1b
hesha <hesha@DESKTOP-VUQL4BG>
2021-05-10 10:21:12 +0200

    update

--- U9_Frequenzgang_Vorlage/src/frequenzgang/model/Complex.java
+++ U9_Frequenzgang_Vorlage/src/frequenzgang/model/Complex.java
@@ -21,28 +21,74 @@ public class Complex {
 
 	public Complex(double re) {
 		this(re, 0.0);
-		
+
 	}
 
 	public Complex(Complex z) {
 		this(z.re, z.im);
 	}
 
-	
 	public String toString() {
 		return ("Re: " + this.re + " Im: " + this.im);
 	}
 
-	
+	public Complex add(Complex z) {
+//		Complex zahl = new Complex();
+//		zahl.re = this.re + z.re;
+//		zahl.im = this.im + z.im;
+//		
+//		return zahl;z.
+		return new Complex(this.re+z.re,this.im + z.im);
+	}
+
+	public Complex sub(Complex z) {
+
+		return null;
+	}
+
+	public Complex mul(double z) {
+
+		return null;
+	}
+
+	public Complex mul(Complex z) {
+
+		return null;
+	}
+
+	public Complex div(double z) {
+
+		return null;
+	}
+
+	public Complex div(Complex z) {
+
+		return null;
+	}
+
+	public Complex power(double x) {
+
+		return null;
+	}
+
+	public double angle() {
+		return 0;
+		 
+	}
+
+	public double abs() {
+		return 0;
+		 
+	}
+
+	public double [] angle(Complex[] c) {
+		return null;
+		 
+	}
 
 	public static void main(String[] args) {
 		Complex c1 = new Complex(2, 4);
 		Complex c2 = new Complex(5, -3);
-		
-		
-		
-
-		
 
 	}
 }

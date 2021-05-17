2713d8ea0e621ace8904f3f9aa9e585d12a34452
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2021-05-17 17:22:35 +0200

    add method as a demo

--- demo_exception/src/demo_exception/Demo1.java
+++ demo_exception/src/demo_exception/Demo1.java
@@ -1,6 +1,22 @@
 package demo_exception;
 
 public class Demo1 {
-	
+
+	public static int getIntValue() {
+		String number = "4.23";
+		try {
+			return Integer.parseInt(number);
+
+		} catch (NumberFormatException exception) {
+			 System.out.println("Error in parseInt");
+			 return 0;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int number = getIntValue();
+		System.out.println(number);
+	}
 
 }
